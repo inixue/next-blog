903fe3e19fd4c8a9cddc8bbc378526451016449a
unknown <1075361899@qq.com>
2019-12-29 21:54:25 +0800

    1

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import {Button} from 'antd'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <div><Button>按钮</Button></div>
+  </div>
+)
+
+export default Home
